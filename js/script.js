d2037a8fb0e0cd616c41a35cc5ebed6ee5e054a8
Dara Carolan <dara.carolan@accenture.com>
2018-04-11 22:09:00 +0100

    basic functionality, info on page

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// console.log("working!!!!");
+
+const APIKEY = "82f2229c255b1505c496b13203e48e52";
+const BASE = "https://api.openweathermap.org/data/2.5/forecast";
+const CITYID = "?id=2964574";
+const UNITS = "&units=metric";
+const URL = `${BASE}${CITYID}${UNITS}&APPID=${APIKEY}`;
+
+function getDay(longDate) {
+  let date = new Date(longDate);
+  let day = date.toString().substring(0, 3);
+  return day;
+}
+
+fetch(URL).then(response => {
+  response.json().then(res => {
+    //header
+    document.getElementsByTagName("header")[0].innerHTML = `Weather in
+    ${res.city.name}, ${res.city.country}`;
+    //day
+    let forecast = res.list;
+    // let date = new Date(forecast[0].dt_txt);
+    // let day = date.toString().substring(0, 3);
+    let day = getDay(forecast[0].dt_txt);
+    document.getElementById("today-day").innerHTML = day;
+    //temp
+    document.getElementById("today-temp").innerHTML = `${
+      forecast[0].main.temp
+    }&#176;`;
+
+    let icon = `<i class="wi wi-${forecast[0].weather[0].icon}"></i>`;
+    document.getElementById("today-icon").innerHTML = icon;
+    document.getElementById("today-description").innerHTML =
+      forecast[0].weather[0].description;
+    document.getElementById(
+      "today-minMax"
+    ).innerHTML = `<div class="details">Max: ${
+      forecast[0].main.temp_max
+    }&#176; | Min: ${forecast[0].main.temp_min}&#176;</div>`;
+
+    // next days forecast
+
+    let data = [];
+    for (let i = 8; i < forecast.length; i += 8) {
+      data.push(forecast[i]);
+    }
+    data.map((item, index) => {
+      //content for elements
+      let day = getDay(item.dt_txt);
+
+      //create elements
+      let aside = document.getElementById("aside");
+      let section = document.createElement("section");
+      let dayDiv = `<div class="today-day">${day}</div>`;
+      let temp = `<div id="today-temp">${item.main.temp}&#176;</div>`;
+      let icon = `<div class="icon"><i class="wi wi-${
+        item.weather[0].icon
+      }"></i></div>`;
+      let description = `<div class="description">${
+        item.weather[0].description
+      }</div>`;
+      let minMax = `<div class="details">Max: ${
+        item.main.temp_max
+      }&#176; | Min: ${item.main.temp_min}&#176;</div>`;
+
+      section.innerHTML = dayDiv + temp + icon + description + minMax;
+      section.setAttribute("class", `forecast-${index + 1}`);
+      aside.append(section);
+    });
+  });
+});
